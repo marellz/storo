2f2da4ed215eae91ccbaa65d5c52f68dbe886efa
Dave Njoroge <d.njoroge@thecodingmachine.com>
2023-10-19 12:15:42 +0300

    move font import from scss to config

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400;500;600;700;800&display=swap" },
+      ],
+    },
+  },
   devtools: { enabled: true },
   postcss: {
     plugins: {
