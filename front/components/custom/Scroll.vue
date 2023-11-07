515224cfe4130c50f069888c41f6595dc7b9e63f
Dave Njoroge <d.njoroge@thecodingmachine.com>
2023-11-07 19:35:59 +0300

    add perfect-scrollbar and scroll component

---
+++ front/components/custom/Scroll.vue
@@ -0,0 +1,13 @@
+<template>
+  <perfect-scrollbar class="overflow-auto pr-5" :class="[maxHeight]">
+    <slot />
+  </perfect-scrollbar>
+</template>
+<script lang="ts" setup>
+defineProps({
+  maxHeight: {
+    type: String,
+    default: "max-h-full",
+  },
+});
+</script>
