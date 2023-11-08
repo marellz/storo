3ed3c20f2e268af1e387df4fc44f882f1c42a1fc
Dave Njoroge <d.njoroge@thecodingmachine.com>
2023-11-08 19:22:37 +0300

    update auth layout responsive design

--- front/layouts/auth.vue
+++ front/layouts/auth.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex flex-col lg:flex-row lg:justify-center items-center lg:h-[70vh]">
+  <div class="flex flex-col lg:flex-row lg:justify-center items-center lg:h-[70vh] space-y-10 pt-10">
     <div>
        <nuxt-link to="/">
           <img
             src="@/assets/images/logos/logo-black.png"
-            class="h-32 lg:h-64"
+            class="h-16 md:h-32"
             alt=""
           />
         </nuxt-link>
     </div>
-    <div class="px-5 md:w-1/2 lg:w-1/3 md:max-w-[500px] min-w-[320px] lg:border-l">
+    <div class="px-5 w-2/3 md:w-1/2 lg:w-1/3 md:max-w-[500px] min-w-[320px] lg:border-l">
       <slot />
     </div>
   </div>
